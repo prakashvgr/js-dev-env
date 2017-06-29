479b6f9c3714beebfae86ccdea50f1d0fc6a0ebd
Prakash Rajendran <prakash.rajendran@capgemini.com>
2017-06-29 16:46:06 +0530

    Delete action included

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {getUsers} from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 getUsers().then(result => {
   let usersBody = '';
 
   result.forEach(user => {
-    usersBody+= `<tr>
+    usersBody += `<tr>
     <td><a href='#' data-id="${user.id}" class="deleteUser">Delete</a></td>
     <td>${user.id}</td>
     <td>${user.firstName}</td>
@@ -16,4 +16,18 @@ getUsers().then(result => {
 
   global.document.getElementById('users').innerHTML = usersBody;
 
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+  
+  /* Must use Array.from to create a real array from DOM collection
+     getElementsByClassName only returns an "array like" object */
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
 });
